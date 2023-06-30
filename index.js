4af04913168dbd0c21386f993916b36ceb7a7394
Martha Suleiman <suleimanmartha15@gmail.com>
2023-06-30 23:23:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+
+// HOME
+let scoreHome = 0
+function add1(){
+let homeScore = document.getElementById("score-home")
+scoreHome  += 1
+homeScore.innerText= scoreHome
+console.log(score)
+}
+function add2(){
+let homeScore = document.getElementById("score-home")
+scoreHome += 2
+homeScore.innerText= scoreHome
+console.log(score)
+}
+function add3(){
+let homeScore = document.getElementById("score-home")
+scoreHome += 3
+homeScore.innerText= scoreHome
+console.log(score)
+}
+
+// GUEST
+let score = 0
+function add1Guest(){
+let homeScore = document.getElementById("score-guest")
+score +=1
+homeScore.innerText= score
+}
+
+function add2Guest(){
+let homeScore = document.getElementById("score-guest")
+score +=  2
+homeScore.innerText= score
+}
+
+function add3Guest(){
+let homeScore = document.getElementById("score-guest")
+score += 3
+homeScore.innerText= score
+
+}
